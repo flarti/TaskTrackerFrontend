8dd4901c35eed6bb8d8baabddcd48307ef50befa
Orest Shkirei <orestshkirei@gmail.com>
2025-09-09 23:09:21 +0300

    first commit

---
+++ src/components/Todo.jsx
@@ -0,0 +1,63 @@
+import React, { useRef, useState } from 'react'
+import TodoItems from './TodoItems'
+
+const Todo = () => {
+
+const[todoList, setTodoList] = useState([]);
+
+const inputRef = useRef();
+
+const add = () =>{
+    const inputText = inputRef.current.value.trim();
+    
+    if(inputText === "") {
+        return null;
+    }
+
+    const newTodo = {
+        id: Date.now(),
+        text: inputText,
+        isComplete: false,
+    }
+
+    setTodoList((prev)=> [...prev, newTodo]);
+    inputRef.current.value = "";
+}
+
+  return (
+    <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
+    
+{/* -------title-------- */}
+
+    <div className='flex items-center mt-3 gap-2'>
+        <h1 className='text-3xl font-semibold'>To-Do List</h1>
+    </div>
+
+{/* -------input-------- */}
+
+    <div className='flex item-center my-7 bg-gray-200 rounded-full'>
+        <input
+        ref={inputRef}
+        className='bg-tranparent botder-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' 
+        type="text" 
+        placeholder='Add your task'
+        />
+
+        <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD</button>
+    </div>
+
+
+{/* -------task list-------- */}
+
+    <div className=''>
+        {todoList.map((item, index)=> {
+            return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete}/>
+        })}
+    </div>
+
+    </div>
+
+  )
+}
+
+export default Todo
---
+++ src/components/TodoItems.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import tick from '../assets/check.png'
+import not_tick from '../assets/checkbox.png'
+import deleteIcon from '../assets/delete.png'
+
+const TodoItems = ({text, id, isComplete}) => {
+  return (
+    <div className='flex items-center my-3 gap-2'>   
+        <div className='flex flex-1 items-center cursor-pointer'>
+            <img src={tick} alt="" className='w-7'/>
+            <p className='tex-slate-700 ml-4 text-[17px]'>
+                {text}
+            </p>
+        </div>
+
+        <img src={deleteIcon} alt="" className='w-7 cursor-pointer'/>
+
+    </div>
+  )
+}
+
+export default TodoItems
