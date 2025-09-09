d7b1e38452123fe902f9e4d5c7102437bf04f51f
Orest Shkirei <orestshkirei@gmail.com>
2025-09-09 23:37:16 +0300

    added isComplete

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -3,12 +3,12 @@ import tick from '../assets/check.png'
 import not_tick from '../assets/checkbox.png'
 import deleteIcon from '../assets/delete.png'
 
-const TodoItems = ({text, id, isComplete, deleteTodo}) => {
+const TodoItems = ({text, id, isComplete, deleteTodo, toggle}) => {
   return (
-    <div className='flex items-center my-3 gap-2'>   
+    <div onClick={()=>{toggle(id)}} className='flex items-center my-3 gap-2'>   
         <div className='flex flex-1 items-center cursor-pointer'>
-            <img src={tick} alt="" className='w-7'/>
-            <p className='tex-slate-700 ml-4 text-[17px]'>
+            <img src={isComplete ? tick : not_tick} alt="" className='w-7'/>
+            <p className={`tex-slate-700 ml-4 text-[17px] decoration-slate-500 ${isComplete ? "line-through" : ""}`}>
                 {text}
             </p>
         </div>
